518653186f04cf307aa52ae985be3a9d126f7b77
JayDee-Tet <jaydeeveea@gmail.com>
2026-09-08 19:09:44 +0800

    Fix formatting issues in script.js

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ const subjects = [
     instructions: 'Read and execute all the codes in the file that I will send po for our asynchronous class today po and for your quiz next meeting.',
     links:        [],
     deadline:     new Date('2026-09-09T23:59:00'),
+  }
 ];
 
 
